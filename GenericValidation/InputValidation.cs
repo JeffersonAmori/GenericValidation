c93de1e059059569b966d96fa20eb1e370966b1c
jeffe <jeffe@DESKTOP-IMSJI3K>
2021-10-24 05:13:49 +0100

    Add project files.

---
+++ GenericValidation/InputValidation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GenericValidation
+{
+    static class InputValidation
+    {
+        static public T GenericReadValidation<T>(string message) where T : struct
+        {
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.WriteLine(message);
+
+                string input = Console.ReadLine();
+                isValid = input.TryParse(out T validatedOutput);
+
+                if (isValid)
+                {
+                    return validatedOutput;
+                }
+                else
+                {
+                    ShowErrorMessage();
+                }
+            }
+
+            throw new ApplicationException("Method shouldn't reach this point. Something terribly wrong happended.");
+        }
+
+        public static bool TryParse<T>(this string input, out T output)
+        {
+            var type = typeof(T);
+            var temp = default(T);
+            var method = type.GetMethod(
+                "TryParse",
+                new[]
+                    {
+                        typeof (string),
+                        Type.GetType(string.Format("{0}&", type.FullName))
+                    });
+
+            output = temp;
+            return (bool)method.Invoke(null, new object[] { input, temp });
+        }
+
+        private static void ShowErrorMessage()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("Entrada inválida. Tente novamente\n");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ GenericValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = InputValidation.GenericReadValidation<int>("Input the age");
+            float salary = InputValidation.GenericReadValidation<float>("Input the salary");
+        }
+    }
+
+    
+}
